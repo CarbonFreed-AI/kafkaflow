068b8aa6e09cfdad2d026b10bac06f0ac4bedbad
Manuel Eisenschink <manuel.eisenschink@carbonfreed.com>
2025-01-27 15:25:41 +0100

    refactor: made JsonCoreSerializer use the new async APIs

--- src/KafkaFlow.Serializer.JsonCore/JsonCoreSerializer.cs
+++ src/KafkaFlow.Serializer.JsonCore/JsonCoreSerializer.cs
@@ -10,7 +10,6 @@ namespace KafkaFlow.Serializer;
 public class JsonCoreSerializer : ISerializer
 {
     private readonly JsonSerializerOptions _serializerOptions;
-    private readonly JsonWriterOptions _writerOptions;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="JsonCoreSerializer"/> class.
@@ -24,38 +23,14 @@ public class JsonCoreSerializer : ISerializer
     /// <summary>
     /// Initializes a new instance of the <see cref="JsonCoreSerializer"/> class.
     /// </summary>
-    /// <param name="writerOptions">Json writer options</param>
-    public JsonCoreSerializer(JsonWriterOptions writerOptions)
-    {
-        _writerOptions = writerOptions;
-    }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="JsonCoreSerializer"/> class.
-    /// </summary>
-    /// <param name="serializerOptions">Json serializer options</param>
-    /// <param name="writerOptions">Json writer options</param>
-    public JsonCoreSerializer(JsonSerializerOptions serializerOptions, JsonWriterOptions writerOptions)
-    {
-        _serializerOptions = serializerOptions;
-        _writerOptions = writerOptions;
-    }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="JsonCoreSerializer"/> class.
-    /// </summary>
     public JsonCoreSerializer()
-        : this(new JsonSerializerOptions(), default)
+        : this(new JsonSerializerOptions())
     {
     }
 
     /// <inheritdoc/>
-    public Task SerializeAsync(object message, Stream output, ISerializerContext context)
+    public async Task SerializeAsync(object message, Stream output, ISerializerContext context)
     {
-        using var writer = new Utf8JsonWriter(output, _writerOptions);
-
-        JsonSerializer.Serialize(writer, message, _serializerOptions);
-
-        return Task.CompletedTask;
+        await JsonSerializer.SerializeAsync(output, message, _serializerOptions).ConfigureAwait(false);
     }
 }
